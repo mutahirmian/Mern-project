5b462db2a7acc850e8c5d775d16356eebaed54c6
mutahirmian <117197924+mutahirmian@users.noreply.github.com>
2024-10-18 21:55:32 +0500

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,34 +1,36 @@
-import React, { useState } from 'react'
-import Navbar from './components/Navbar/Navbar'
-import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home/Home'
-import Cart from './pages/Cart/Cart'
-import PlaceOrder from './pages/PlaceOrder/PlaceOrder' 
-import Footer from './components/Footer/Footer'
-import LoginPopup from './components/LoginPopup/LoginPopup'
-import Verify from './pages/Verify/Verify page'
-import MyOrders from './pages/MyOrders/MyOrdersPage'
+import { useState } from 'react';
+import Navbar from './components/Navbar/Navbar';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Cart from './pages/Cart/Cart';
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder'; 
+import Footer from './components/Footer/Footer';
+import LoginPopup from './components/LoginPopup/LoginPopup';
+import Verify from './pages/Verify/Verify page';
+import MyOrders from './pages/MyOrders/MyOrdersPage';
+import { ToastContainer } from 'react-toastify'; // Import ToastContainer
+import 'react-toastify/dist/ReactToastify.css'; // Import CSS for toast notifications
 
 const App = () => {
+    const [showLogin, setShowLogin] = useState(false);
 
-   const [showLogin, setShowLogin] = useState(false)
+    return (
+        <>
+            {showLogin ? <LoginPopup setShowLogin={setShowLogin} /> : <></>}
+            <div className='app'>
+                <Navbar setShowLogin={setShowLogin} />
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/cart' element={<Cart />} />
+                    <Route path='/order' element={<PlaceOrder />} />
+                    <Route path='/verify' element={<Verify />} />
+                    <Route path='/myorders' element={<MyOrders />} />
+                </Routes>
+            </div>
+            <Footer />
+            <ToastContainer />
+        </>
+    );
+};
 
-  return (
-    <>
-    {showLogin?<LoginPopup setShowLogin={setShowLogin} />:<></>}
-    <div className='app'>
-      <Navbar setShowLogin={setShowLogin}/>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/cart' element={<Cart/>}/>
-        <Route path='/order' element={<PlaceOrder/>}/>
-        <Route path='/verify' element={<Verify />}/>
-        <Route path='/myorders' element={<MyOrders />}/>
-      </Routes>
-      </div>
-      <Footer />
-      </>
-  )
-}
-
-export default App
+export default App;
